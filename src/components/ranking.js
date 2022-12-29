1483846cb96a0462cbc0175f592d5b48142c1c83
Kevin Luis <k.luis360@gmail.com>
2022-12-29 17:55:32 -0300

    feat: adding exoteric function on ranking mode

--- src/components/ranking.js
+++ src/components/ranking.js
@@ -1,10 +1,96 @@
+import { useEffect, useState } from "react";
+
+import { cavalos } from "../utils/mocks";
+
+
 const Ranking = () => {
+  const [horseToSearch, setHorseToSearch] = useState(undefined);
+  const [horseFound, setHorseFound] = useState();
+
+  function calculate(a) {
+    if (a.length < 250) {
+      const randomIndex = Math.floor(Math.random() * (a.length - 0)) + 0;
+      return a[randomIndex];
+    } else {
+      const groups = [];
+      for (let i = 0; i < 5; i++) {
+        groups[i] = [];
+      }
+      for (let i = 0; i < 25; i += 5) {
+        groups[1].push(a[i]);
+        groups[2].push(a[i + 1]);
+        groups[3].push(a[i + 2]);
+        groups[4].push(a[i + 3]);
+        groups[5].push(a[i + 4]);
+      }
+
+      for (let i = 0; i < 5; i++) {
+        groups[i].sort(function (a, b) {
+          return a - b;
+        });
+      }
+
+      const medians = [];
+
+      for (let i = 0; i < 5; i++) {
+        medians.push(groups[i][2]);
+      }
+      medians.sort(function (a, b) {
+        return a - b;
+      });
+
+      return medians[2];
+    }
+  }
+
+  function select(a, k) {
+    const median = calculate(a);
+    const right = [];
+    const left = [];
+    a.forEach((element) => {
+      if (element.volta < median.volta) left.push(element);
+      else if (element.volta > median.volta) right.push(element);
+    });
+    if (left.length === k - 1) return median;
+    if (left.length > k - 1) return select(left, k);
+    else return select(right, k - left.length - 1);
+  }
+
+  function search(posicao) {
+    if (posicao > cavalos.length)
+      return alert("Posição maior do que o número de cavalos");
+    if (posicao === undefined) return alert("Digite uma posição válida");
+    const cavalo = select(cavalos, posicao);
+    console.log(cavalo);
+    setHorseFound(cavalo);
+  }
+
+  useEffect(() => {}, [horseToSearch]);
   return (
     <div className="ranking">
-      <h1>Ranking</h1>
+      <h1>Competidores</h1>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        {cavalos.map((cavalo, index) => {
+          return (
+            <div key={index} style={{ display: "flex" }}>
+              <span>{cavalo.nome + ": " + cavalo.volta} </span>
+            </div>
+          );
+        })}
+      </div>
+      <div className="search-horse">
+        <h2>Escolha a posição</h2>
+        <input
+          type="number"
+          onChange={(event) => {
+            setHorseToSearch(event.target.value);
+          }}
+        ></input>
+        <button onClick={() => search(horseToSearch)}>Buscar</button>
+      </div>
+      {horseFound?.nome !== "" && <span>{horseFound?.nome}</span>}
     </div>
   );
 };
-
 
 export default Ranking;
---
+++ src/utils/mocks.js
@@ -0,0 +1,78 @@
+export const cavalos = [
+    {
+      nome: "Pegasus",
+      volta: 4.31,
+    },
+    {
+      nome: "Pesadelo",
+      volta: 4.91,
+    },
+    {
+      nome: "Galã",
+      volta: 4.13,
+    },
+    {
+      nome: "Fergus",
+      volta: 4.14,
+    },
+    {
+      nome: "Pé de pano",
+      volta: 4.51,
+    },
+    {
+      nome: "Atlas",
+      volta: 4.29,
+    },
+    {
+      nome: "Sansão",
+      volta: 4.83,
+    },
+    {
+      nome: "Jato",
+      volta: 4.64,
+    },
+    {
+      nome: "Alazão",
+      volta: 4.61,
+    },
+    {
+      nome: "Marvin",
+      volta: 4.23,
+    },
+    {
+      nome: "Valente",
+      volta: 4.01,
+    },
+    {
+      nome: "Donatelo",
+      volta: 4.32,
+    },
+    {
+      nome: "Raio",
+      volta: 4.98,
+    },
+    {
+      nome: "Atrevido",
+      volta: 4.99,
+    },
+    {
+      nome: "Pompeu",
+      volta: 5.01,
+    },
+    {
+      nome: "Carbonero",
+      volta: 4.51,
+    },
+    {
+      nome: "Elegante",
+      volta: 4.87,
+    },
+    {
+      nome: "Dengoso",
+      volta: 4.74,
+    },
+    {
+      nome: "Gaspar",
+      volta: 5.11,
+    },
+  ];
