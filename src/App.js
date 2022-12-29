c4349e9a1c11073bb27d3e26a99ba5bd8b5c46ea
Kevin Luis <k.luis360@gmail.com>
2022-12-29 15:41:22 -0300

    feat: adding start menu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/app.css";
+import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState(1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <span onClick={() => setMode(1)}>menu</span>
       </header>
+      <div className="main-menu">
+        {mode === 1 && (
+          <div className="choose-div">
+            <h3> Escolha a função que voce deseja</h3>
+            <div className="options">
+              <span
+                onClick={() => {
+                  setMode(2);
+                }}
+              >
+                Verificar a posição dos cavalos
+              </span>
+              <span
+                onClick={() => {
+                  setMode(3);
+                }}
+              >
+                Verificar histórico de posições dos cavalos e buscar qual foi melhor em qual volta
+              </span>
+            </div>
+          </div>
+        )}
+        {mode === 2 && <span>Kevin</span>}
+        {mode === 3 && <span>Ailton</span>}
+      </div>
     </div>
   );
 }
