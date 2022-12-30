75c553030d5f5682287283a5822b76be93533bcf
Kevin Luis <k.luis360@gmail.com>
2022-12-30 17:56:56 -0300

    feat: adding img atribrute to cavalos mock

--- src/utils/mocks.js
+++ src/utils/mocks.js
@@ -1,78 +1,97 @@
 export const cavalos = [
-    {
-      nome: "Pegasus",
-      volta: 4.31,
-    },
-    {
-      nome: "Pesadelo",
-      volta: 4.91,
-    },
-    {
-      nome: "Galã",
-      volta: 4.13,
-    },
-    {
-      nome: "Fergus",
-      volta: 4.14,
-    },
-    {
-      nome: "Pé de pano",
-      volta: 4.51,
-    },
-    {
-      nome: "Atlas",
-      volta: 4.29,
-    },
-    {
-      nome: "Sansão",
-      volta: 4.83,
-    },
-    {
-      nome: "Jato",
-      volta: 4.64,
-    },
-    {
-      nome: "Alazão",
-      volta: 4.61,
-    },
-    {
-      nome: "Marvin",
-      volta: 4.23,
-    },
-    {
-      nome: "Valente",
-      volta: 4.01,
-    },
-    {
-      nome: "Donatelo",
-      volta: 4.32,
-    },
-    {
-      nome: "Raio",
-      volta: 4.98,
-    },
-    {
-      nome: "Atrevido",
-      volta: 4.99,
-    },
-    {
-      nome: "Pompeu",
-      volta: 5.01,
-    },
-    {
-      nome: "Carbonero",
-      volta: 4.51,
-    },
-    {
-      nome: "Elegante",
-      volta: 4.87,
-    },
-    {
-      nome: "Dengoso",
-      volta: 4.74,
-    },
-    {
-      nome: "Gaspar",
-      volta: 5.11,
-    },
-  ];
+  {
+    nome: "Pegasus",
+    volta: 4.31,
+    img: "https://st.depositphotos.com/1003660/1320/i/600/depositphotos_13209068-stock-photo-beautiful-brown-horse-running-trot.jpg",
+  },
+  {
+    nome: "Pesadelo",
+    volta: 4.91,
+    img: "https://img.freepik.com/fotos-gratis/lindo-cavalo-castanho_144627-19417.jpg?w=2000",
+  },
+  {
+    nome: "Galã",
+    volta: 4.13,
+    img: "https://i.pinimg.com/550x/2b/b8/19/2bb819bd8571af52a261fb0202921d50.jpg",
+  },
+  {
+    nome: "Fergus",
+    volta: 4.14,
+    img: "https://img.freepik.com/fotos-gratis/lindo-cavalo-castanho-close-up-do-focinho-aparencia-bonita-juba-plano-de-fundo-do-campo-de-atletismo-curral-arvores-cavalos-sao-animais-maravilhosos_639032-566.jpg?w=2000",
+  },
+  {
+    nome: "Pé de pano",
+    volta: 4.51,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR00iN6zg7qjwCWJmd8T6yaW3zwsluQxWFhca9YVWJtHA&s",
+  },
+  {
+    nome: "Atlas",
+    volta: 4.29,
+    img: "https://media.istockphoto.com/id/501965551/pt/foto/foto-apresenta%C3%A7%C3%A3o-o-galloping-cavalo-branco.jpg?s=612x612&w=0&k=20&c=NIb-Krrx8r8xuDablznGemfp2cAdc7PfpQD-mh64sY8=",
+  },
+  {
+    nome: "Sansão",
+    volta: 4.83,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS591YLcylK8eis4lGe9pQah6DTdw1_ss3IaX2JnEg&s",
+  },
+  {
+    nome: "Jato",
+    volta: 4.64,
+    img: "https://i.pinimg.com/236x/73/df/ce/73dfce6690fa98aa9165a56395f54b51.jpg",
+  },
+  {
+    nome: "Alazão",
+    volta: 4.61,
+    img: "https://thumbs.dreamstime.com/b/cavalo-que-eleva-acima-137013869.jpg",
+  },
+  {
+    nome: "Marvin",
+    volta: 4.23,
+    img: "https://st.depositphotos.com/1003660/1320/i/600/depositphotos_13209068-stock-photo-beautiful-brown-horse-running-trot.jpghttps://thumbs.dreamstime.com/b/cria%C3%A7%C3%A3o-de-cavalos-%C3%A1rabes-cinzentos-em-contexto-turvo-encrenca-cinzenta-%C3%A1rabe-sobre-um-fundo-nublado-azul-e-rosa-no-deserto-200397254.jpg",
+  },
+  {
+    nome: "Valente",
+    volta: 4.01,
+    img: "https://thumbs.dreamstime.com/b/garanh%C3%A3o-vermelho-que-eleva-acima-97437489.jpg",
+  },
+  {
+    nome: "Donatelo",
+    volta: 4.32,
+    img: "https://i.pinimg.com/236x/a3/92/0f/a3920fe4e900a1a28065ba64a872df91--the-black-stallion-free-photos.jpg",
+  },
+  {
+    nome: "Raio",
+    volta: 4.98,
+    img: "https://static4.depositphotos.com/1013693/313/i/600/depositphotos_3131098-stock-photo-brown-horse-portrait.jpg",
+  },
+  {
+    nome: "Atrevido",
+    volta: 4.99,
+    img: "https://static8.depositphotos.com/1009314/896/i/450/depositphotos_8963370-stock-photo-concerned-horse.jpg",
+  },
+  {
+    nome: "Pompeu",
+    volta: 5.01,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSainjklst-Ic_88mOrOvddJX9nF-0MrntyZI0vfarzoQ&s",
+  },
+  {
+    nome: "Carbonero",
+    volta: 4.51,
+    img: "https://static.wixstatic.com/media/416c85_de66afba950246a99f772f3385256cd7~mv2_d_2250_1500_s_2.jpg/v1/fill/w_984,h_656,al_c,q_85,usm_0.66_1.00_0.01/416c85_de66afba950246a99f772f3385256cd7~mv2_d_2250_1500_s_2.webp",
+  },
+  {
+    nome: "Elegante",
+    volta: 4.87,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8oBRfkH3K2FjXmHUwf4oWqi4M9NiLogtAFaQLjOZx&s",
+  },
+  {
+    nome: "Dengoso",
+    volta: 4.74,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEmPh9qL94jc-3HRVv5q_CURawNBfyo-u7VEJsNVPE&s",
+  },
+  {
+    nome: "Gaspar",
+    volta: 5.11,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSd69dtNaxnHP0IQQBMFFd5AwoflxcsILYb2x1Vg4YoVS6TsLWgtIUDrgNvhWzM1d43Yw&usqp=CAU",
+  },
+];
